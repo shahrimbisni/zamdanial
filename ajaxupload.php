83b8414d43371c29353bcce7b43bfa8c8be985c4
shahrimbisni <shahrimbisni@gmail.com>
2018-11-24 19:39:29 +0800

    sample upload file

---
+++ ajaxupload.php
@@ -0,0 +1,42 @@
+<?php
+
+$valid_extensions = array('jpeg', 'jpg', 'png', 'gif', 'bmp' , 'pdf' , 'doc' , 'ppt'); // valid extensions
+$path = 'database/uploads/'; // upload directory
+
+if(!empty($_POST['name']) || !empty($_POST['email']) || $_FILES['file'])
+{
+	$file = $_FILES['file']['name'];
+	$tmp = $_FILES['file']['tmp_name'];
+
+	// get uploaded file's extension
+	$ext = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+
+	// can upload same image using rand function
+	$final_image = rand(1000,1000000).$file;
+
+	// check's valid format
+	if(in_array($ext, $valid_extensions)) 
+	{	 
+		$path = $path.strtolower($final_image); 
+
+		if(move_uploaded_file($tmp,$path)) 
+		{
+		echo "Uploaded file Succesfully!";
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+
+		//include database configuration file
+		include_once 'database/db.php';
+
+		//insert form data in the database
+		$insert = $db->query("INSERT uploading (name,email,file_name) VALUES ('".$name."','".$email."','".$path."')");
+
+		//echo $insert?'ok':'err';
+		}
+	} 
+	else 
+	{
+	echo 'invalid';
+	}
+}
+?>
--- database/js/script.js
+++ database/js/script.js
@@ -1,35 +1,35 @@
 $(document).ready(function (e) {
- $("#form").on('submit',(function(e) {
-  e.preventDefault();
-  $.ajax({
-         url: "ajaxupload.php",
-   type: "POST",
-   data:  new FormData(this),
-   contentType: false,
-         cache: false,
-   processData:false,
-   beforeSend : function()
-   {
-    //$("#preview").fadeOut();
-    $("#err").fadeOut();
-   },
-   success: function(data)
+  $("#form").on('submit',(function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: "ajaxupload.php",
+      type: "POST",
+      data:  new FormData(this),
+      contentType: false,
+      cache: false,
+      processData:false,
+      beforeSend : function()
       {
-    if(data=='invalid')
-    {
-     // invalid file format.
-     $("#err").html("Invalid File !").fadeIn();
-    }
-    else
-    {
-     // view uploaded file.
-     $("#preview").html(data).fadeIn();
-     $("#form")[0].reset(); 
-    }
+        //$("#preview").fadeOut();
+        $("#err").fadeOut();
       },
-     error: function(e) 
+      
+      success: function(data)
       {
-    $("#err").html(e).fadeIn();
+        if(data=='invalid')
+        {
+          // invalid file format.
+          $("#err").html("Invalid File !").fadeIn();
+        } else {
+          // view uploaded file.
+          $("#preview").html(data).fadeIn();
+          $("#form")[0].reset(); 
+        }
+      },
+      error: function(e) 
+      {
+
+        $("#err").html(e).fadeIn();
       }          
     });
  }));
