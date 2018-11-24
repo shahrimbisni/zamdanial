4428cade74fee3dd48a8706113157305e4c10c2d
shahrimbisni <shahrimbisni@gmail.com>
2018-11-24 22:25:02 +0800

    email

--- ajaxupload.php
+++ ajaxupload.php
@@ -26,15 +26,37 @@ if(!empty($_POST['name']) || !empty($_POST['email']) || $_FILES['file'])
 		$email = $_POST['email'];
 
 		//sent email notification
-		$email_to = "shahrimbisni@gmail.com";
-    	$email_subject = "New Application From : ". $name;
-    	$email_message = "Form details below.\n\n";
-
-    	// create email headers
-		$headers = 'From: '.$email."\r\n".
+		if(isset($_POST['email'])) {
+ 
+		    // EDIT THE 2 LINES BELOW AS REQUIRED
+		    $email_to = "shahrimbisni@gmail.com";
+		    $email_subject = "Your email subject line";
+		 
+		    function died($error) {
+		        // your error code can go here
+		        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+		        echo "These errors appear below.<br /><br />";
+		        echo $error."<br /><br />";
+		        echo "Please go back and fix these errors.<br /><br />";
+		        die();
+		    }		  
+		 
+		    $email_message = "Form details below.\n\n";
+		 
+		     
+		    function clean_string($string) {
+		      $bad = array("content-type","bcc:","to:","cc:","href");
+		      return str_replace($bad,"",$string);
+		    }
+		 
+		     
+		 
+		// create email headers
+		$headers = 'From: '.$name."\r\n".
 		'Reply-To: '.$email."\r\n" .
 		'X-Mailer: PHP/' . phpversion();
 		@mail($email_to, $email_subject, $email_message, $headers);
+	}
 
 		//include database configuration file
 		include_once 'database/db.php';
