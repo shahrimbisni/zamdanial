666ea0119d307655842512516e6612458faf3612
shahrimbisni <shahrimbisni@gmail.com>
2018-12-03 18:15:40 +0800

    Required field for payslip

--- database/ajaxupload.php
+++ database/ajaxupload.php
@@ -4,33 +4,64 @@ $valid_extensions = array('jpeg', 'jpg', 'png', 'gif', 'bmp' , 'pdf' , 'doc' , '
 
 $path1 = 'copy_ic/'; // upload directory
 $path2 = 'copy_lesen/'; // upload directory
+$path3 = 'payslip/'; // upload directory
 
-if(!empty($_POST['name']) || !empty($_POST['email']) || !empty($_POST['phone']) || !empty($_POST['address']) || !empty($_POST['car_model']) || !empty($_POST['car_color']) || $_FILES['copy_ic'] || $_FILES['copy_lesen'] || !empty($_POST['status_permohonan']))
-{
+
+if ($_POST['status_permohonan'] == 'gaji_tetap') {
+	if ($_FILES['payslip1_3bulan']['error'] > 0) { 
+    	echo 'payslip1_3bulan_required';
+	} else if ($_FILES['payslip2_3bulan']['error'] > 0) {
+		echo 'payslip2_3bulan_required';
+	} else if ($_FILES['payslip3_3bulan']['error'] > 0) {
+		echo 'payslip3_3bulan_required';
+	}
+} else if ($_POST['status_permohonan'] == 'gaji_tak_tetap') {
+	if ($_FILES['payslip1_6bulan']['error'] > 0) { 
+    	echo 'payslip1_6bulan_required';
+	} else if ($_FILES['payslip2_6bulan']['error'] > 0) { 
+    	echo 'payslip2_6bulan_required';
+	} else if ($_FILES['payslip3_6bulan']['error'] > 0) { 
+    	echo 'payslip3_6bulan_required';
+	} else if ($_FILES['payslip4_6bulan']['error'] > 0) { 
+    	echo 'payslip4_6bulan_required';
+	} else if ($_FILES['payslip5_6bulan']['error'] > 0) { 
+    	echo 'payslip5_6bulan_required';
+	} else if ($_FILES['payslip6_6bulan']['error'] > 0) { 
+    	echo 'payslip6_6bulan_required';
+	}
+} else {
+
 	$copy_ic = $_FILES['copy_ic']['name'];
 	$tmp_copy_ic = $_FILES['copy_ic']['tmp_name'];
 
 	$copy_lesen = $_FILES['copy_lesen']['name'];
 	$tmp_copy_lesen = $_FILES['copy_lesen']['tmp_name'];
 
+	$payslip1_3bulan = $_FILES['payslip1_3bulan']['name'];
+	$tmp_payslip1_3bulan = $_FILES['payslip1_3bulan']['tmp_name'];
+
 	// get uploaded file's extension
 	$ext_copy_ic = strtolower(pathinfo($copy_ic, PATHINFO_EXTENSION));
 	$ext_copy_lesen = strtolower(pathinfo($copy_lesen, PATHINFO_EXTENSION));
+	$ext_payslip1_3bulan = strtolower(pathinfo($payslip1_3bulan, PATHINFO_EXTENSION));
 
 	// can upload same image using rand function
 	$copy_ic = rand(1000,1000000).$copy_ic;
 	$copy_lesen = rand(1000,1000000).$copy_lesen;
+	$payslip1_3bulan = rand(1000,1000000).$payslip1_3bulan;
 
 	// check's valid format
-	if((in_array($ext_copy_ic, $valid_extensions)) && (in_array($ext_copy_lesen, $valid_extensions)))
+	if((in_array($ext_copy_ic, $valid_extensions)) && (in_array($ext_copy_lesen, $valid_extensions)) && (in_array($ext_payslip1_3bulan, $valid_extensions)))
 	{	 
 		$path1 = $path1.strtolower($copy_ic); 
 		$path2 = $path2.strtolower($copy_lesen);
+		$path3 = $path3.strtolower($payslip1_3bulan);
 
 		$copy_ic = strtolower($copy_ic); 
 		$copy_lesen = strtolower($copy_lesen); 
+		$copy_payslip1_3bulan = strtolower($payslip1_3bulan); 
 
-		if((move_uploaded_file($tmp_copy_ic,$path1)) && (move_uploaded_file($tmp_copy_lesen,$path2)))
+		if((move_uploaded_file($tmp_copy_ic,$path1)) && (move_uploaded_file($tmp_copy_lesen,$path2)) && (move_uploaded_file($tmp_payslip1_3bulan,$path3)))
 		{
 		// echo "Uploaded file Succesfully!";
 		$name = $_POST['name'];
@@ -90,7 +121,7 @@ if(!empty($_POST['name']) || !empty($_POST['email']) || !empty($_POST['phone']) 
 		include_once 'db.php';
 
 		//insert form data in the database
-		$insert = $db->query("INSERT uploading (name,email,phone,address,car_model,car_color,mesej,copy_ic,copy_lesen,status_permohonan) VALUES ('".$name."','".$email."','".$phone."','".$address."','".$car_model."','".$car_color."','".$mesej."','".$copy_ic."','".$copy_lesen."','".$status_permohonan."')");
+		$insert = $db->query("INSERT uploading (name,email,phone,address,car_model,car_color,mesej,copy_ic,copy_lesen,status_permohonan,payslip1) VALUES ('".$name."','".$email."','".$phone."','".$address."','".$car_model."','".$car_color."','".$mesej."','".$copy_ic."','".$copy_lesen."','".$status_permohonan."','".$payslip1_3bulan."')");
 
 		//echo $insert?'ok':'err';
 		echo 'success';
@@ -101,4 +132,5 @@ if(!empty($_POST['name']) || !empty($_POST['email']) || !empty($_POST['phone']) 
 		echo 'invalid';
 	}
 }
+
 ?>
--- database/js/script.js
+++ database/js/script.js
@@ -20,6 +20,18 @@ $(document).ready(function (e) {
         {
           // invalid file format.
           $("#message").html("Invalid File !").fadeIn();
+        } else if ((data=='payslip1_3bulan_required') || (data=='payslip1_6bulan_required')) {
+          $("#message").html("Payslip1 is Required !").fadeIn();
+        } else if ((data=='payslip2_3bulan_required') || (data=='payslip2_6bulan_required')) {
+          $("#message").html("Payslip2 is Required !").fadeIn();
+        } else if ((data=='payslip3_3bulan_required') || (data=='payslip3_6bulan_required')) {
+          $("#message").html("Payslip3 is Required !").fadeIn();
+        } else if (data=='payslip4_6bulan_required') {
+          $("#message").html("Payslip4 is Required !").fadeIn();
+        } else if (data=='payslip5_6bulan_required') {
+          $("#message").html("Payslip5 is Required !").fadeIn();
+        } else if (data=='payslip6_6bulan_required') {
+          $("#message").html("Payslip6 is Required !").fadeIn();
         } else if (data=='success') {
           $("#message").html("Uploaded File Succesfully! !").fadeIn();
           $("#form")[0].reset(); 
